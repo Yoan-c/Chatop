6193dfc5984c50303ce409e13ce325a43eeec307
ti-metiss <coutenay.yoan@hotmail.fr>
2023-09-18 11:12:35 +0200

    separate file config

--- src/main/java/com/example/rentals/configuration/ApplicationConfig.java
+++ src/main/java/com/example/rentals/configuration/ApplicationConfig.java
@@ -1,11 +1,8 @@
 package com.example.rentals.configuration;
 
 import com.example.rentals.repository.UserRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -47,15 +44,7 @@ public class ApplicationConfig {
         return config.getAuthenticationManager();
     }
 
-    @Bean
-    public ModelMapper modelMapper(){
-        return  new ModelMapper();
-    }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-        configurer.setLocation(new FileSystemResource("./.env"));
-        return configurer;
-    }
+
+
 }
--- src/main/java/com/example/rentals/configuration/SecurityConfig.java
+++ src/main/java/com/example/rentals/configuration/SecurityConfig.java
@@ -51,7 +51,5 @@ public class SecurityConfig {
             "/swagger-ui.html",
             "/documentation",
             "/documentation-api"
-
-
     };
 }
